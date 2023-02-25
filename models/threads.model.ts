8348559a5cfcc1e6568b4472f24e7ffdc7403549
RHLance <lance.endaya@rewardholdings.com>
2023-02-25 08:03:21 +0800

    fix: add timestamps to threads

--- models/threads.model.ts
+++ models/threads.model.ts
@@ -1,76 +1,79 @@
 import mongoose from "mongoose";
 import { isCorrectLength } from "../utils/validators";
 
-const threadSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-    validate: [
-      {
-        validator: (title: string) => isCorrectLength(title, 5, "min"),
-        message: "Title must have atleast 5 characters",
-      },
-      {
-        validator: (title: string) => isCorrectLength(title, 150, "max"),
-        message: "Title must be less than or equal to 150 characters",
-      },
-    ],
-  },
-  description: {
-    type: String,
-    required: true,
-    validate: [
-      {
-        validator: (title: string) => isCorrectLength(title, 5, "min"),
-        message: "Description must have atleast 20 characters",
-      },
-      {
-        validator: (title: string) => isCorrectLength(title, 600, "max"),
-        message: "Title must be less than or equal to 600 characters",
-      },
-    ],
-  },
-  createdBy: {
-    id: {
-      type: String || null,
-      required: false,
+const threadSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      validate: [
+        {
+          validator: (title: string) => isCorrectLength(title, 5, "min"),
+          message: "Title must have atleast 5 characters",
+        },
+        {
+          validator: (title: string) => isCorrectLength(title, 150, "max"),
+          message: "Title must be less than or equal to 150 characters",
+        },
+      ],
     },
-    userName: {
+    description: {
+      type: String,
+      required: true,
+      validate: [
+        {
+          validator: (title: string) => isCorrectLength(title, 5, "min"),
+          message: "Description must have atleast 20 characters",
+        },
+        {
+          validator: (title: string) => isCorrectLength(title, 600, "max"),
+          message: "Title must be less than or equal to 600 characters",
+        },
+      ],
+    },
+    createdBy: {
+      id: {
+        type: String || null,
+        required: false,
+      },
+      userName: {
+        type: String,
+        required: true,
+      },
+      avatar: {
+        type: String || null,
+        required: false,
+      },
+    },
+    createdFor: {
+      id: {
+        type: String,
+        required: true,
+      },
+      userName: {
+        type: String,
+        required: true,
+      },
+      avatar: {
+        type: String || null,
+        required: false,
+      },
+    },
+    status: {
       type: String,
       required: true,
     },
-    avatar: {
-      type: String || null,
-      required: false,
-    },
-  },
-  createdFor: {
-    id: {
-      type: String,
+    isAnonymous: {
+      type: Boolean,
       required: true,
     },
-    userName: {
-      type: String,
+    isLocked: {
+      type: Boolean,
       required: true,
     },
-    avatar: {
-      type: String || null,
-      required: false,
-    },
   },
-  status: {
-    type: String,
-    required: true,
-  },
-  isAnonymous: {
-    type: Boolean,
-    required: true,
-  },
-  isLocked: {
-    type: Boolean,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const Threads = mongoose.model("Threads", threadSchema);
 
